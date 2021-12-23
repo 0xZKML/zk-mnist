ffbe35ad6db4cd69ee7dd92561fccf292375159e
henripal <henri.palacci@gmail.com>
2021-12-23 14:48:39 -0500

    using embeddings and conv layers

--- src/App.js
+++ src/App.js
@@ -9,12 +9,16 @@ import { generateProof, buildContractCallArgs } from "./snarkUtils";
 import path from 'path';
 import './App.css';
 // import Token from './artifacts/contracts/Token.sol/Token.json'
+import { Tensor, InferenceSession } from "onnxruntime-web";
+
+
 
 const verifierAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
 
 function App() {
     //   const [greeting, setGreetingValue] = useState()
-    const [image, setImage] = useState("")
+    const [image, setImage] = useState([])
+    const [quantizedEmbedding, setQuantizedEmbedding] = useState([])
     const [proof, setProof] = useState("")
     const [publicSignal, setPublicSignal] = useState()
     const [isVerified, setIsVerified] = useState(false);
@@ -28,10 +32,10 @@ function App() {
     }
 
     async function publishProof() {
-        let imageVec = image.slice(1,-1);
-        imageVec = imageVec.split(', ') // array of strings of numbers
         if (typeof window.ethereum !== 'undefined') {
-            const { proof, publicSignals } = await generateProof(imageVec)
+          console.log('generate proof with')
+          console.log(quantizedEmbedding)
+            const { proof, publicSignals } = await generateProof(quantizedEmbedding)
             setPublicSignal(publicSignals);
             setProof(proof);
         }
@@ -54,6 +58,35 @@ function App() {
         }    
     }
 
+    async function calcEmbedding() {
+      console.log(image)
+      const session = await InferenceSession.create(
+        "http://localhost:3000/trimmed_convet.onnx",
+        {
+          executionProviders: ["wasm"],
+        }
+      );
+      const data = Float32Array.from(image)
+      console.log(data)
+      const tensor = new Tensor('float32', data, [1, 1, 28, 28]);
+      console.log(tensor)
+      const feeds = { input: tensor};
+      const results = await session.run(feeds);
+      const embeddingResult = results.output.data;
+      console.log(embeddingResult)
+      var tempQuantizedEmbedding = new Array(50)
+      for (var i = 0; i < 50; i++)
+        tempQuantizedEmbedding[i] = parseInt((embeddingResult[i]*1000).toFixed()) + 10000;
+
+      setQuantizedEmbedding(tempQuantizedEmbedding)
+    }
+
+    async function myReset() {
+      setGrid(Array(size).fill(null).map(_=> Array(size).fill(0)));
+      setImage([])
+
+    }
+
     function handleChangeData(myrow,mycol){
         // console.log('handleChangeData ',grid[0]);
         var newArray = [];
@@ -62,19 +95,7 @@ function App() {
         newArray[myrow][mycol]=1;
         setGrid(newArray);
 
-        var out='[';
-        for(var i=0;i<size; i++) {
-            for(var j=0;j<size;j++) {
-                if(!(i==size-1 && j==size-1)){
-                    out = out + newArray[i][j] + ', ';
-                }
-                else {
-                    out = out + newArray[i][j] + ']';
-                }
-            }
-        }
-        setImage(out);
-        console.log('in handleChangeData, '+out);
+        setImage(newArray.flat());
     }
 
 
@@ -83,6 +104,14 @@ function App() {
       <MNISTBoard onChange={(r,c) => handleChangeData(r,c)}  />
 
       <header className="App-header">
+        <p></p>
+        <button onClick={myReset}>
+        Reset Data
+        </button>
+        <p></p>
+        <button onClick={calcEmbedding}>
+           Compute Embeddings 
+        </button>
         <p></p>
         <button onClick={publishProof}>
             Capture and publish proof
