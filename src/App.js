95d171dbda61778adda0b5d6fdf5e30a5aeb8cdd
Francis Ho <ho.francis@gmail.com>
2022-03-18 21:55:56 -0700

    1

--- src/App.js
+++ src/App.js
@@ -17,8 +17,8 @@ import {digSize} from './MNISTDigits.js';
 
 var image=[]; // the image array will eventually be a flattened version of grid (the 2-dim array)
 // const verifierAddress = "0x5FbDB2315678afecb367f032d93F642f64180aa3"
-const verifierAddress = "0x1C29c8A3C98f4a2f97a02a71D19a2D50Ac1a5a5D"
-
+// const verifierAddress = "0x1C29c8A3C98f4a2f97a02a71D19a2D50Ac1a5a5D"
+const verifierAddress = "0xAA2487705E138e4dd37dF2e4851bFC8984872721"
 var selectedImgUrl="";
 
 function App() {
